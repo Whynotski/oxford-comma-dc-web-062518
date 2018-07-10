51bdc7246ef105d9d87c39dbc03b1010659e95b0
whynotski <laurawynosky@gmail.com>
2018-07-10 16:15:13 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 1
+  return "#{array[0]}"
+  elsif array.length == 2
+  return "#{array[0]} and #{array[1]}"
+  else array.length >= 3
+  array[array.length - 1] = ("and #{array.last}")
+  array.join (", ")
+  end
 end
